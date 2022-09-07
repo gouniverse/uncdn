1e515051f956a97da0c15e016467b5d46644e24d
Milan Lesichkov <lesichkovm@gmail.com>
2022-09-07 21:07:35 +0100

    Update Vue3_test.go

--- Vue3_test.go
+++ Vue3_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-func TestVue3(t *testing.T) {
-	output := Vue3()
+func TestVueJs3(t *testing.T) {
+	output := VueJs3()
 	expected := "Vue"
 	if !strings.Contains(output, expected) {
 		t.Error("Does not contain '" + expected + "', Output:" + output)
