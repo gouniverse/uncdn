7b7be7f1b3af15ac7938014e5133b172f8f598b1
Milan Lesichkov <lesichkovm@gmail.com>
2022-09-08 03:37:50 +0000

    Updates

--- PicoCss153.go
+++ PicoCss153.go
@@ -3,5 +3,5 @@ package uncdn
 import "github.com/gouniverse/uncdn/templates"
 
 func PicoCss153() string {
-	return templates.ToString("templates/vendor/picocss/pico-1.5.3.min.js")
+	return templates.ToString("templates/vend/picocss/pico-1.5.3.min.js")
 }
