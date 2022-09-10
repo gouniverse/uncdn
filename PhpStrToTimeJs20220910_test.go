3790e20be2282d65a5718f9d0a48c105120a1f31
Milan Lesichkov <lesichkovm@gmail.com>
2022-09-10 17:34:56 +0100

    Update PhpStrToTimeJs20220910_test.go

--- PhpStrToTimeJs20220910_test.go
+++ PhpStrToTimeJs20220910_test.go
@@ -1,1 +1,16 @@
 package uncdn
+
+package uncdn
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestPhpStrToTimeJs20220910_test(t *testing.T) {
+	output := PhpStrToTimeJs20220910()
+	expected := "strtotime"
+	if !strings.Contains(output, expected) {
+		t.Error("Does not contain '" + expected + "', Output:" + output)
+	}
+}
