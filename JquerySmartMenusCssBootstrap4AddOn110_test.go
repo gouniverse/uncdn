f7849b85e189dfa5800fa5272f479f9ddbb86599
Milan Lesichkov <lesichkovm@gmail.com>
2022-09-11 22:25:20 +0100

    Update JquerySmartMenusCssBootstrap4AddOn110_test.go

--- JquerySmartMenusCssBootstrap4AddOn110_test.go
+++ JquerySmartMenusCssBootstrap4AddOn110_test.go
@@ -1,1 +1,14 @@
+package uncdn
 
+import (
+	"strings"
+	"testing"
+)
+
+func TestJquerySmartMenusCssBootstrap4AddOn110(t *testing.T) {
+	output := JquerySmartMenusCssBootstrap4AddOn110()
+	expected := ".navbar-nav.sm-collapsible"
+	if !strings.Contains(output, expected) {
+		t.Error("Does not contain '" + expected + "', Output:" + output)
+	}
+}
