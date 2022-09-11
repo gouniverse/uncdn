307e37bd72d2a2ff2c129ab5fc5c9e5eb66f5775
Milan Lesichkov <lesichkovm@gmail.com>
2022-09-11 22:04:11 +0100

    Update JquerySmartMenusCss110_test.go

--- JquerySmartMenusCss110_test.go
+++ JquerySmartMenusCss110_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestJquerySmartMenusCss110(t *testing.T) {
 	output := JquerySmartMenusCss110()
-	expected := ".SmartMenus"
+	expected := ".sm"
 	if !strings.Contains(output, expected) {
 		t.Error("Does not contain '" + expected + "', Output:" + output)
 	}
