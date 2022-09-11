56141d824e1958795ac952956aeaa068b28524c0
Milan Lesichkov <lesichkovm@gmail.com>
2022-09-11 21:56:05 +0100

    Update and rename JquerySmartMenuJs110_test.go to JquerySmartMenusJs110_test.go

--- JquerySmartMenusJs110_test.go
+++ JquerySmartMenusJs110_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-func TestJquerySmartMenuJs110(t *testing.T) {
-	output := JquerySmartMenuJs110()
+func TestJquerySmartMenusJs110(t *testing.T) {
+	output := JquerySmartMenusJs110()
 	expected := ".container,.container-lg,.container-md,.container-sm,.container-xl"
 	if !strings.Contains(output, expected) {
 		t.Error("Does not contain '" + expected + "', Output:" + output)
