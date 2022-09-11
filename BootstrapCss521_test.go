d96e3d7a04d89d590bc911e5422f3e714eecf5f1
Milan Lesichkov <lesichkovm@gmail.com>
2022-09-11 21:49:18 +0100

    Update BootstrapCss521_test.go

--- BootstrapCss521_test.go
+++ BootstrapCss521_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestBootstrapCss521(t *testing.T) {
 	output := BootstrapCss521()
-	expected := "Bootstrap  v5.2.1"
+	expected := ".container,.container-lg,.container-md,.container-sm,.container-xl"
 	if !strings.Contains(output, expected) {
 		t.Error("Does not contain '" + expected + "', Output:" + output)
 	}
