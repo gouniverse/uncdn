9404b0a4fe1bccdffaa2830c267fdb85bfe841d1
Milan Lesichkov <lesichkovm@gmail.com>
2022-09-11 21:45:51 +0100

    Update JquerySmartMenuJs110.go

--- JquerySmartMenuJs110.go
+++ JquerySmartMenuJs110.go
@@ -2,6 +2,6 @@ package uncdn
 
 import "github.com/gouniverse/uncdn/templates"
 
-func Jquery360() string {
-	return templates.ToString("vend/jquery-smartmenus/jquery-3.6.0.min.js")
+func JquerySmartMenus110() string {
+	return Raw("jquery-smartmenus/jquery.smartmenus.min.js")
 }
