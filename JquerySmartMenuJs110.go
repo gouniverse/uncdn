1486ec5e3688a8f900ef7ee7a060661af8d90769
Milan Lesichkov <lesichkovm@gmail.com>
2022-09-11 21:55:34 +0100

    Update JquerySmartMenuJs110.go

--- JquerySmartMenuJs110.go
+++ JquerySmartMenuJs110.go
@@ -1,5 +1,5 @@
 package uncdn
 
-func JquerySmartMenuJs110() string {
-	return Raw("jquery-smartmenus/jquery.smartmenus.min.js")
+func JquerySmartMenusJs110() string {
+	return Raw("jquery-smartmenus/1.1.0/jquery.smartmenus.min.js")
 }
