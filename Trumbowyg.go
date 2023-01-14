0c1e0df4ab73c4bac6fb5644a4b46c64756ab00b
Milan Lesichkov <lesichkovm@gmail.com>
2023-01-14 19:06:42 +0000

    Update Trumbowyg.go

--- Trumbowyg.go
+++ Trumbowyg.go
@@ -2,11 +2,11 @@ package uncdn
 
 import "github.com/gouniverse/uncdn/templates"
 
-func TrumbowygCss() string {
+func Trumbowyg2260Css() string {
 	return templates.ToString("vend/trumbowyg/2.26.0/trumbowyg.min.css")
 }
 
 
-func TrumbowygJs() string {
+func Trumbowyg2260Js() string {
 	return templates.ToString("vend/trumbowyg/2.26.0/trumbowyg.min.js")
 }
